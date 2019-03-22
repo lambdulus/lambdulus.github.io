ee14f4120a74292060f509bf15fce2545300586a
jalivert <184747279+jalivert@users.noreply.github.com>
2019-03-22 12:42:10 +0100

    first build

---
+++ precache-manifest.574b59966e86a4fcde1f384a2eee2c05.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "efcf02813825d9752b58",
+    "url": "/static/js/main.efcf0281.chunk.js"
+  },
+  {
+    "revision": "58c4bcde38a282202746",
+    "url": "/static/js/2.58c4bcde.chunk.js"
+  },
+  {
+    "revision": "efcf02813825d9752b58",
+    "url": "/static/css/main.f5be5029.chunk.css"
+  },
+  {
+    "revision": "bba5ccf3d77b50f234d851ac29d8000b",
+    "url": "/index.html"
+  }
+];
