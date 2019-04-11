dfabfb448f5a64d6e2a1b44f93635e5a55599734
jalivert <184747279+jalivert@users.noreply.github.com>
2019-04-11 16:40:16 +0200

    new deployment

---
+++ precache-manifest.6fdd81b2de7fc1727805e3e65ee24652.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "649ea2f528d8f114dd8f",
+    "url": "/static/js/main.649ea2f5.chunk.js"
+  },
+  {
+    "revision": "dad04721fc897d95d6b3",
+    "url": "/static/js/2.dad04721.chunk.js"
+  },
+  {
+    "revision": "649ea2f528d8f114dd8f",
+    "url": "/static/css/main.f5be5029.chunk.css"
+  },
+  {
+    "revision": "eba1095594883572ab5f73441edfa16f",
+    "url": "/index.html"
+  }
+];
