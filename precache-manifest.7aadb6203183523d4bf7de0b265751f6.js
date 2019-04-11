c173a42eb00d0e2bd7a29cc37971aa6d96968163
Taskkill <johny95wil@gmail.com>
2019-04-11 19:56:36 +0200

    release with breakpoints

---
+++ precache-manifest.7aadb6203183523d4bf7de0b265751f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "fe62385e03135940caa6",
+    "url": "/static/js/main.fe62385e.chunk.js"
+  },
+  {
+    "revision": "5f98a9701190f60149b0",
+    "url": "/static/js/2.5f98a970.chunk.js"
+  },
+  {
+    "revision": "fe62385e03135940caa6",
+    "url": "/static/css/main.f5be5029.chunk.css"
+  },
+  {
+    "revision": "e64eaf6084d4e23c8f3d2696e10723da",
+    "url": "/index.html"
+  }
+];
