6a523ee4c74b0a9c19106b847e12b5fcfe586366
Taskkill <johny95wil@gmail.com>
2019-04-21 17:27:51 +0200

    add list syntax

---
+++ precache-manifest.598489009115527f4a8ea4b867710dfa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "97df4a0debb02938696e",
+    "url": "/static/js/main.97df4a0d.chunk.js"
+  },
+  {
+    "revision": "24fe57a70f8498afc555",
+    "url": "/static/js/2.24fe57a7.chunk.js"
+  },
+  {
+    "revision": "97df4a0debb02938696e",
+    "url": "/static/css/main.f5be5029.chunk.css"
+  },
+  {
+    "revision": "ebe247a639d3ef0e3c3b9507042b2312",
+    "url": "/index.html"
+  }
+];
