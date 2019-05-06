65f4bcb01be35c6fb7e5223fe9ef2311172f695d
Taskkill <johny95wil@gmail.com>
2019-05-06 19:51:00 +0200

    next

---
+++ precache-manifest.3dfbd9ef77ff9a1ad7d39991dc8ffd05.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "907ab2174dad8c70edb2",
+    "url": "/static/js/main.907ab217.chunk.js"
+  },
+  {
+    "revision": "9e05e2a4c5f6e1584761",
+    "url": "/static/js/2.9e05e2a4.chunk.js"
+  },
+  {
+    "revision": "907ab2174dad8c70edb2",
+    "url": "/static/css/main.f5be5029.chunk.css"
+  },
+  {
+    "revision": "91be45e647044e1f013066cded0b8b72",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d8ebb0068eb088e2b0790d645f2d5c1e.js"
+  "/precache-manifest.3dfbd9ef77ff9a1ad7d39991dc8ffd05.js"
 );
 
 workbox.clientsClaim();
