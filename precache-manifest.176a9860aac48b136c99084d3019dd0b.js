500cdda12947b1e092bc86065373dd0cbdcbb470
Taskkill <johny95wil@gmail.com>
2019-05-13 23:01:59 +0200

    lovely colors

--- precache-manifest.176a9860aac48b136c99084d3019dd0b.js
+++ precache-manifest.176a9860aac48b136c99084d3019dd0b.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.fdfcfda2.js"
   },
   {
-    "revision": "907ab2174dad8c70edb2",
-    "url": "/static/js/main.907ab217.chunk.js"
+    "revision": "498850e519f32fd52a53",
+    "url": "/static/js/main.498850e5.chunk.js"
   },
   {
     "revision": "9e05e2a4c5f6e1584761",
     "url": "/static/js/2.9e05e2a4.chunk.js"
   },
   {
-    "revision": "907ab2174dad8c70edb2",
+    "revision": "498850e519f32fd52a53",
     "url": "/static/css/main.f5be5029.chunk.css"
   },
   {
-    "revision": "91be45e647044e1f013066cded0b8b72",
+    "revision": "f9df2120f497d26a32762c4de7c21956",
     "url": "/index.html"
   }
 ];
