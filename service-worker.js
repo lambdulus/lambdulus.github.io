668af83d63aa8771d6fbf37824eeb3c59154c64d
Taskkill <johny95wil@gmail.com>
2019-07-01 23:30:31 +0200

    next

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.73798add63bcaf706502a03bb6f4b433.js"
+  "/precache-manifest.ee2beea8afa220aa3eaef82e6aa11274.js"
 );
 
 workbox.clientsClaim();
