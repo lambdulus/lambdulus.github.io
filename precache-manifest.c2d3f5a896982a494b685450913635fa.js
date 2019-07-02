ca51e4b459909e02d1cba445e8e9dbda4b57af36
Taskkill <johny95wil@gmail.com>
2019-07-02 14:50:27 +0200

    next

---
+++ precache-manifest.c2d3f5a896982a494b685450913635fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "54e8011736d1a08655de",
+    "url": "/static/js/main.54e80117.chunk.js"
+  },
+  {
+    "revision": "7807214579f6e1be2009",
+    "url": "/static/js/2.78072145.chunk.js"
+  },
+  {
+    "revision": "54e8011736d1a08655de",
+    "url": "/static/css/main.52cf77a6.chunk.css"
+  },
+  {
+    "revision": "bdf7d69da643a0d85e5db54f9821fd14",
+    "url": "/index.html"
+  }
+];
