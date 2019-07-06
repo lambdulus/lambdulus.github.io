527146c6aff5c0a33c141ed7fa000a48b3b7e6bb
jalivert <184747279+jalivert@users.noreply.github.com>
2019-07-06 22:43:48 +0200

    next

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c37ef481c36626b25e178103bd6b9489.js"
+  "/precache-manifest.94c661fae11be61ec3de510ee0d1ba41.js"
 );
 
 workbox.clientsClaim();
