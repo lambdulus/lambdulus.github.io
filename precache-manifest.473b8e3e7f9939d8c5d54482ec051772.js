aaf499f42e1848018387878b3a018e7b1f3aa02e
Taskkill <johny95wil@gmail.com>
2019-08-23 12:25:42 +0200

    next

---
+++ precache-manifest.473b8e3e7f9939d8c5d54482ec051772.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "41577b53cf6f8b46e6bf",
+    "url": "/static/js/main.41577b53.chunk.js"
+  },
+  {
+    "revision": "ca0d48cca3735f7b811e",
+    "url": "/static/js/2.ca0d48cc.chunk.js"
+  },
+  {
+    "revision": "41577b53cf6f8b46e6bf",
+    "url": "/static/css/main.72e9573e.chunk.css"
+  },
+  {
+    "revision": "ca0d48cca3735f7b811e",
+    "url": "/static/css/2.df7e7786.chunk.css"
+  },
+  {
+    "revision": "8ae16ff6cf891c8cef350ab78a0ec9c8",
+    "url": "/index.html"
+  }
+];
