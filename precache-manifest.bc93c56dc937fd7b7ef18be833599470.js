910fc1d02d40ac8e246496aa94cf87dae006275c
Taskkill <johny95wil@gmail.com>
2019-09-19 15:03:42 +0200

    next

---
+++ precache-manifest.bc93c56dc937fd7b7ef18be833599470.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "6c4980868b1c77a53956",
+    "url": "/static/js/main.6c498086.chunk.js"
+  },
+  {
+    "revision": "bf7c45687a3c57733208",
+    "url": "/static/js/2.bf7c4568.chunk.js"
+  },
+  {
+    "revision": "6c4980868b1c77a53956",
+    "url": "/static/css/main.f251c9b4.chunk.css"
+  },
+  {
+    "revision": "bf7c45687a3c57733208",
+    "url": "/static/css/2.df7e7786.chunk.css"
+  },
+  {
+    "revision": "f4160af5b38903a72c8e91deeb95d8e3",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.944ce6cd7c0fb36b38668fdfaaddd32d.js"
+  "/precache-manifest.bc93c56dc937fd7b7ef18be833599470.js"
 );
 
 workbox.clientsClaim();
