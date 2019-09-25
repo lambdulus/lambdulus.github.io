18901c69567ed65ab70a4fe51364a0eb9cfae758
jalivert <184747279+jalivert@users.noreply.github.com>
2019-09-25 22:38:44 +0200

    next

--- precache-manifest.55ab2fc1b7acb439f912aa126b3972fe.js
+++ precache-manifest.55ab2fc1b7acb439f912aa126b3972fe.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.fdfcfda2.js"
   },
   {
-    "revision": "215698b1d18a620bfc4f",
-    "url": "/static/js/main.215698b1.chunk.js"
+    "revision": "d0abd0dea5d7f4b1bd65",
+    "url": "/static/js/main.d0abd0de.chunk.js"
   },
   {
     "revision": "167c77380aff17a37728",
     "url": "/static/js/2.167c7738.chunk.js"
   },
   {
-    "revision": "215698b1d18a620bfc4f",
-    "url": "/static/css/main.769838cd.chunk.css"
+    "revision": "d0abd0dea5d7f4b1bd65",
+    "url": "/static/css/main.8e03d84c.chunk.css"
   },
   {
     "revision": "167c77380aff17a37728",
     "url": "/static/css/2.df7e7786.chunk.css"
   },
   {
-    "revision": "a41a21584612e3ca19ef3cc406c6418b",
+    "revision": "7990922da705c1a5c1c1a3b98bded8b2",
     "url": "/index.html"
   }
 ];
