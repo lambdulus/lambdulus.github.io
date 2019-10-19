4ef464e66d0b8c77a2314b7a37a4a702b4fd1b0c
Taskkill <johny95wil@gmail.com>
2019-10-19 18:49:36 +0200

    next

--- precache-manifest.0cf4b59530493ed58cf2ca003b205298.js
+++ precache-manifest.0cf4b59530493ed58cf2ca003b205298.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.fdfcfda2.js"
   },
   {
-    "revision": "606bf1b96e8636275dc4",
-    "url": "/static/js/main.606bf1b9.chunk.js"
+    "revision": "9ca17fb41e75d645e01e",
+    "url": "/static/js/main.9ca17fb4.chunk.js"
   },
   {
     "revision": "b60aef56b47b9b6ec661",
     "url": "/static/js/2.b60aef56.chunk.js"
   },
   {
-    "revision": "606bf1b96e8636275dc4",
+    "revision": "9ca17fb41e75d645e01e",
     "url": "/static/css/main.ae5656a9.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/static/css/2.df7e7786.chunk.css"
   },
   {
-    "revision": "0677df518e00326fd256bdd05a80ec63",
+    "revision": "269c1c6311583dfaea185aa6aab15c07",
     "url": "/index.html"
   }
 ];
