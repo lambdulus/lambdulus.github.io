bf0c70d1b80df316b604e3836ca4071ba9009df0
Taskkill <johny95wil@gmail.com>
2020-08-26 13:24:43 +0200

    hopefully fixing what previous commit should've fixed

---
+++ precache-manifest.689f17047a205a31050d2a7409a22086.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94f62fb5f07e6c96f7246e5831e0d6d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92221eed1df02f8fdce8",
+    "url": "/static/css/2.b507a811.chunk.css"
+  },
+  {
+    "revision": "222472b7ba72a807b02d",
+    "url": "/static/css/main.ec17ea8a.chunk.css"
+  },
+  {
+    "revision": "92221eed1df02f8fdce8",
+    "url": "/static/js/2.b68ea5e7.chunk.js"
+  },
+  {
+    "revision": "2225c84c946de3dbee439e7e753521ae",
+    "url": "/static/js/2.b68ea5e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "222472b7ba72a807b02d",
+    "url": "/static/js/main.75f903ae.chunk.js"
+  },
+  {
+    "revision": "6312e0cd8a827abdb337",
+    "url": "/static/js/runtime-main.80b62565.js"
+  }
+]);
