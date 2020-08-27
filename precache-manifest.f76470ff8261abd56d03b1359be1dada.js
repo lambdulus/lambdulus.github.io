ef8328335958adf51c49bb13b7710bd53cb97db5
Taskkill <johny95wil@gmail.com>
2020-08-27 02:04:15 +0200

    next: SDE-highlighting + auto-scroll + other fixes and updates

---
+++ precache-manifest.f76470ff8261abd56d03b1359be1dada.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfe9c33d81db38eee174507d1e1371e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb3ef691a63e7e90d9d9",
+    "url": "/static/css/2.b507a811.chunk.css"
+  },
+  {
+    "revision": "2fa821141b1f7a2c64e3",
+    "url": "/static/css/main.62be5022.chunk.css"
+  },
+  {
+    "revision": "eb3ef691a63e7e90d9d9",
+    "url": "/static/js/2.d2bf7fa0.chunk.js"
+  },
+  {
+    "revision": "2225c84c946de3dbee439e7e753521ae",
+    "url": "/static/js/2.d2bf7fa0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fa821141b1f7a2c64e3",
+    "url": "/static/js/main.619994e2.chunk.js"
+  },
+  {
+    "revision": "6312e0cd8a827abdb337",
+    "url": "/static/js/runtime-main.80b62565.js"
+  }
+]);
