c68c883bc99642b78b91b83b0ad55fc53be82942
Taskkill <johny95wil@gmail.com>
2020-08-27 11:25:07 +0200

    next - disable RUN + EXERCISE when SDE enabled

--- precache-manifest.0870c0c5f3357883d25b8c72663f1a5e.js
+++ precache-manifest.0870c0c5f3357883d25b8c72663f1a5e.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "492bccc83409b524795485d3d3a241df",
+    "revision": "1b80d89b0a901eaa4e1aaad78f97bb61",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.b507a811.chunk.css"
   },
   {
-    "revision": "e168d9c16ba183e507f1",
+    "revision": "4ddff3df9c975d7984c0",
     "url": "/static/css/main.62be5022.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.d2bf7fa0.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e168d9c16ba183e507f1",
-    "url": "/static/js/main.432bdb1f.chunk.js"
+    "revision": "4ddff3df9c975d7984c0",
+    "url": "/static/js/main.05c00bf8.chunk.js"
   },
   {
     "revision": "6312e0cd8a827abdb337",
