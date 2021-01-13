64eaaba62e839d5da1c7e5781f8a588864d493af
Taskkill <johny95wil@gmail.com>
2021-01-14 00:00:29 +0100

    rebuild with changed macros

---
+++ precache-manifest.206a6a3f45e16c38365ced871f0f2690.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a10e04e23a7ad77508dd0d63a327e710",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af4e036d02a18e2e8d0d",
+    "url": "/static/css/2.b507a811.chunk.css"
+  },
+  {
+    "revision": "cc4d2f6569c22ebad2c4",
+    "url": "/static/css/main.fcb09c3a.chunk.css"
+  },
+  {
+    "revision": "af4e036d02a18e2e8d0d",
+    "url": "/static/js/2.98c3231c.chunk.js"
+  },
+  {
+    "revision": "2225c84c946de3dbee439e7e753521ae",
+    "url": "/static/js/2.98c3231c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc4d2f6569c22ebad2c4",
+    "url": "/static/js/main.2e6c4249.chunk.js"
+  },
+  {
+    "revision": "20f28f2644c4ac439613",
+    "url": "/static/js/runtime-main.ef08eca9.js"
+  }
+]);
