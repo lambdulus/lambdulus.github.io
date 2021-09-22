06bdaa57c59568e8a1bcc434f03ddbe3eb75a55c
Taskkill <johny95wil@gmail.com>
2021-09-22 17:13:08 +0200

    rebuild - fix bug: non-empty (submitted) boxes shared through the link would fail to import

--- precache-manifest.9dd4a6167d917bea4fdbbb17e7b8ea16.js
+++ precache-manifest.9dd4a6167d917bea4fdbbb17e7b8ea16.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3dfdd574627ee49de0cc8cfd545e353b",
+    "revision": "ee3914650d7c37460d05f26cd00f1a25",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.b507a811.chunk.css"
   },
   {
-    "revision": "f13db6c2c994848e47aa",
+    "revision": "ace37b8ffa573584fbd9",
     "url": "/static/css/main.cc49b5ac.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.dfa9dca5.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f13db6c2c994848e47aa",
-    "url": "/static/js/main.1fd0b4a3.chunk.js"
+    "revision": "ace37b8ffa573584fbd9",
+    "url": "/static/js/main.d8a5aedb.chunk.js"
   },
   {
     "revision": "6bef46007dd996a33074",
