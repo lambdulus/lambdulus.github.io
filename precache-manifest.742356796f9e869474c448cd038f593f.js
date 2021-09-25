d0e3498c2f6acaeb4546fd6c390e7c2b8492a7a8
Taskkill <johny95wil@gmail.com>
2021-09-25 23:54:47 +0200

    fix a bug - sharing a link to box with expression containing macro definitions incorrectly opens the box with the semicolon after the last macro definition missing, causing a syntax error

--- precache-manifest.742356796f9e869474c448cd038f593f.js
+++ precache-manifest.742356796f9e869474c448cd038f593f.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ee3914650d7c37460d05f26cd00f1a25",
+    "revision": "e105c441053615da5893aab434c81c50",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.b507a811.chunk.css"
   },
   {
-    "revision": "ace37b8ffa573584fbd9",
+    "revision": "3342f08520b92b629bd2",
     "url": "/static/css/main.cc49b5ac.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.dfa9dca5.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ace37b8ffa573584fbd9",
-    "url": "/static/js/main.d8a5aedb.chunk.js"
+    "revision": "3342f08520b92b629bd2",
+    "url": "/static/js/main.7299dcaf.chunk.js"
   },
   {
     "revision": "6bef46007dd996a33074",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9dd4a6167d917bea4fdbbb17e7b8ea16.js"
+  "/precache-manifest.742356796f9e869474c448cd038f593f.js"
 );
 
 self.addEventListener('message', (event) => {
