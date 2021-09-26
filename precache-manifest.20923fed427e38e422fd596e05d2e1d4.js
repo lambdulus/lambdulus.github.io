64a076c5bcc43d16be8ce48a2eebb0e13185a0e4
Taskkill <johny95wil@gmail.com>
2021-09-26 23:20:16 +0200

    adding the rest of the build

---
+++ precache-manifest.20923fed427e38e422fd596e05d2e1d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bc4b0ec11afbf8349e9d9d336c2379e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d9f095fd54c6b34ccf7",
+    "url": "/static/css/2.b507a811.chunk.css"
+  },
+  {
+    "revision": "07b774259beac2a7459a",
+    "url": "/static/css/main.a406353b.chunk.css"
+  },
+  {
+    "revision": "0d9f095fd54c6b34ccf7",
+    "url": "/static/js/2.dfa9dca5.chunk.js"
+  },
+  {
+    "revision": "2225c84c946de3dbee439e7e753521ae",
+    "url": "/static/js/2.dfa9dca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07b774259beac2a7459a",
+    "url": "/static/js/main.c03de773.chunk.js"
+  },
+  {
+    "revision": "6bef46007dd996a33074",
+    "url": "/static/js/runtime-main.706a5b09.js"
+  }
+]);
