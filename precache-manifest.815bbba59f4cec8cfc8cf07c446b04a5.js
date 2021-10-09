9633ae45a9dbc0f4cbcc80787166d397e13a0035
jalivert <184747279+jalivert@users.noreply.github.com>
2021-10-09 14:05:00 +0200

    deploy - cleaner UI - better Top Panel

--- precache-manifest.815bbba59f4cec8cfc8cf07c446b04a5.js
+++ precache-manifest.815bbba59f4cec8cfc8cf07c446b04a5.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3399626efc719925b3a6bd60386eb16a",
+    "revision": "ba7416e7f82d21dbbe7d2ae9632bd67a",
     "url": "/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.b507a811.chunk.css"
   },
   {
-    "revision": "6aa5b4bd34cbd4916ab7",
-    "url": "/static/css/main.958f8afe.chunk.css"
+    "revision": "fb69aec683a33a4adcbb",
+    "url": "/static/css/main.31b9a524.chunk.css"
   },
   {
     "revision": "0d9f095fd54c6b34ccf7",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.dfa9dca5.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6aa5b4bd34cbd4916ab7",
-    "url": "/static/js/main.cad7cc81.chunk.js"
+    "revision": "fb69aec683a33a4adcbb",
+    "url": "/static/js/main.87a2ef70.chunk.js"
   },
   {
     "revision": "6bef46007dd996a33074",
