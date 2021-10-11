dc5fc6904780da9d3b84001eb66defd348f51a18
Taskkill <johny95wil@gmail.com>
2021-10-11 14:36:49 +0200

    deploy : UI fixes and clean-ups

---
+++ precache-manifest.1dd3bbaf0781c33f9e239b912747f5b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0578f3894987b6559b8a6d43963c33f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8659bcdebc206554ca90",
+    "url": "/static/css/2.b507a811.chunk.css"
+  },
+  {
+    "revision": "b2869e1828ba406eef8d",
+    "url": "/static/css/main.85805b9f.chunk.css"
+  },
+  {
+    "revision": "8659bcdebc206554ca90",
+    "url": "/static/js/2.999fd6cd.chunk.js"
+  },
+  {
+    "revision": "2225c84c946de3dbee439e7e753521ae",
+    "url": "/static/js/2.999fd6cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2869e1828ba406eef8d",
+    "url": "/static/js/main.db32a00b.chunk.js"
+  },
+  {
+    "revision": "6bef46007dd996a33074",
+    "url": "/static/js/runtime-main.706a5b09.js"
+  }
+]);
