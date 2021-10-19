9b6b8aa5ff9a6cb0c76e2a2adc4862863e27fbcd
Taskkill <johny95wil@gmail.com>
2021-10-19 17:01:53 +0200

    deploy new alpha renamer

---
+++ precache-manifest.a314b1c2bdc1d93aaef282140294fc6c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c8ec36ad06d2bf1f3c566ac902639ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3e99eccd26134775d85",
+    "url": "/static/css/2.7ea9ee01.chunk.css"
+  },
+  {
+    "revision": "957a319aabf1273e05ef",
+    "url": "/static/css/main.862b17ff.chunk.css"
+  },
+  {
+    "revision": "a3e99eccd26134775d85",
+    "url": "/static/js/2.759ec9f3.chunk.js"
+  },
+  {
+    "revision": "cd8f8a436c1dbe11f02ea2290810511b",
+    "url": "/static/js/2.759ec9f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "957a319aabf1273e05ef",
+    "url": "/static/js/main.7be2484f.chunk.js"
+  },
+  {
+    "revision": "b9e73928f30f5afab4b9",
+    "url": "/static/js/runtime-main.99d7ad55.js"
+  },
+  {
+    "revision": "888f9cc09dcbe8c760194f97322a8431",
+    "url": "/static/media/codicon.888f9cc0.ttf"
+  }
+]);
